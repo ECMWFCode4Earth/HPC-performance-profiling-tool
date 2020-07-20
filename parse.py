4be5af93d222e4d47be868b2385d99914da77895
Tiberiu Lepădatu <tiberiulepadatu14@gmail.com>
2020-07-20 14:21:14 +0300

    Save the plots and solve the sunburst

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 import json 
 import pprint 
+import pandas as pd
 
 parsed_dict = {}
 pp = pprint.PrettyPrinter(indent=3)
@@ -9,18 +10,22 @@ keys_keep = ["rrtm_taumol3" ,"solver_mcica_lw" ,"rrtm_kgb7"]
 with open("callgraph.json") as f:
     data = json.load(f)
     # filter the data for easy handeling
-    data = { x : data[x] for x in keys_keep}
+    
+    # data = { x : data[x] for x in keys_keep}
 
-    for i in data.keys()[:1]:
+    for i in data.keys():
         for j in data[i]:
             name, scope, n_file = j.split(" ")
             if j not in parsed_dict:
                 parsed_dict[name] = {}
-                parsed_dict[name]['parents'] = []
                 parsed_dict[name]['scope'] = scope.replace("[","").replace("]","")
                 parsed_dict[name]['file'] = n_file.replace("(","").replace(")","")
                 # name scope and file
-            parsed_dict[name]['parents'].append(i)
-pp.pprint(json.dumps(parsed_dict))
-pp.pprint(parsed_dict)
-            
+            if name == i:
+                parsed_dict[name]['parent'] = None
+            else:
+                parsed_dict[name]['parent'] = i
+df = pd.DataFrame.from_dict(parsed_dict).transpose()
+print(df.head(19))
+print(df.parent)
+#pp.pprint(parsed_dict)
